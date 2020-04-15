410c51bf087c4a7f85d6fefa52f819c0cc92ce68
VANKATA-svg <60429951+VANKATA-svg@users.noreply.github.com>
2020-04-15 16:10:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Conveter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit;
+
+            double celsius = -5.5;
+            Console.WriteLine("Celsius: " + celsius);
+
+            fahrenheit = (celsius * 9) / 5 + 32;
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+
+            Console.ReadLine();
+        }
+    }
+}
